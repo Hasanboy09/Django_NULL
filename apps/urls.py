2f041df42bcf433bd9178f87deafe3ddc4eb1d8c
Hasanboy09 <sanoqulovhasan113@gmail.com>
2024-11-27 17:15:12 +0500

    changed

--- apps/urls.py
+++ apps/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.views import hello_world_view
 
 urlpatterns = [
-    path('hello-world/', hello_world_view, name='hello-world'),
+    path('hello/', hello_world_view, name='hello-world'),
 
 ]
